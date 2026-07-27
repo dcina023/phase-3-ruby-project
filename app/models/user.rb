934b3908b4151ba33c24e4f3e78fd7aefa8114d0
+ <+>
2026-07-27 15:31:08 -0400

    Clean Up User model methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,29 +2,16 @@ class User < ActiveRecord::Base
   has_many :meal_plans
   has_many :meals, through: :meal_plans
 
-  def create_meal_plan(attributes)
-    meal_plans.create(attributes)
-  end
 
-  def users_budget
+  def total_budget
     meal_plans.sum(:budget)
   end
 
-  def favorite_meal
+  def favorite_meals
     meals.where(favorite: true)
   end
 
-  def select_individual_meal
+  def meal_options
     meals.pluck(:id, :name)
-  end
-
-  def find_meal(input)
-    meals.find_by(id: input) || meals.find_by(name: input)
-  end
-
-  def add_favorite_meal(input)
-    meal = find_meal(input)
-    meal.update(favorite: true)
-    meal
   end
 end
