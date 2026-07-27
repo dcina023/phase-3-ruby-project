dd8e5a735899ccb46e0034fc47d1319263f679ec
+ <+>
2026-07-27 18:22:26 -0400

    Add select user feature and remove unused User model methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,17 +1,4 @@
 class User < ActiveRecord::Base
   has_many :meal_plans
   has_many :meals, through: :meal_plans
-
-
-  def total_budget
-    meal_plans.sum(:budget)
-  end
-
-  def favorite_meals
-    meals.where(favorite: true)
-  end
-
-  def meal_options
-    meals.pluck(:id, :name)
-  end
 end
--- cli/cli.rb
+++ cli/cli.rb
@@ -7,7 +7,8 @@ class CLI
   def main_menu
     loop do
       puts "\n1. View users"
-      puts "2. Exit"
+      puts "2. Select user"
+      puts "3 Exit"
       print "Choose an option: "
 
       choice = gets.chomp.downcase
@@ -15,7 +16,9 @@ class CLI
       case choice
       when "1", "users"
         list_users
-      when "2", "exit"
+      when "2", "select"
+        select_user
+      when "3", "exit"
         puts "Goodbye!"
         break
       else
@@ -27,6 +30,19 @@ class CLI
   def list_users
     User.all.each do |user|
       puts "#{user.id}. #{user.name}"
+    end
+  end
+
+  def select_user
+    print "Enter a user ID: "
+    user_id = gets.chomp
+
+    user = User.find_by(id: user_id)
+
+    if user
+      puts "Selected user: #{user.name}"
+    else
+      puts "User not found."
     end
   end
 end
