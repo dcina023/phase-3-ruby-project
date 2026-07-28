0b348d4cf4dd25fb15a6fcc911e317d097416251
+ <+>
2026-07-28 13:31:02 -0400

    Create add_new_meal_plan method for users

--- cli/cli.rb
+++ cli/cli.rb
@@ -38,7 +38,7 @@ class CLI
       when "3", "add user"
         add_new_user
       when "4", "add mealplan"
-        add_mealplan
+        add_new_meal_plan
       when "5", "update mealplan"
         update_mealplan
       when "6", "delete mealplan"
@@ -117,35 +117,51 @@ class CLI
     meal_plan
   end
 
-  def display_meals(meal_plan)
-    if meal_plan.meals.any?
-      meal_plan.meals.each do |meal|
-        puts "  -> Meal: #{meal.name} (ID: #{meal.id})
-          (meal_type: #{meal.meal_type})
-          (prep_time: #{meal.prep_time})
-          (estimated_cost: #{meal.estimated_cost})
-          (calories: #{meal.calories})
-          (prepared: #{meal.prepared}
-          (favorite: #{meal.favorite})"
-      end
+  def add_new_meal_plan
+    user = select_user
+    return unless user
+
+    print "Meal plan name: "
+    name = gets.chomp
+
+    print "Week start date: "
+    week_start = gets.chomp
+
+    print "Goal: "
+    goal = gets.chomp
+
+    print "Budget: "
+    budget = gets.chomp.to_f
+
+    confirmed = @prompt.yes?("Are you sure you want to add '#{name}' meal plan for #{user.name}?")
+
+    if confirmed
+      new_meal_plan = user.meal_plans.create(
+        name: name,
+        week_start: week_start,
+        goal: goal,
+        budget: budget
+      )
+
+      puts "Success! '#{new_meal_plan.name}' has been created."
     else
-      puts "No meals found for this meal plan."
+      puts "Adding meal plan canceled."
     end
   end
 
-  def select_meal(meal_plan)
-    display_meals(meal_plan)
-
-    print "Enter a meal ID: "
-    meal_id = gets.chomp
-
-    meal = meal_plan.meals.find { |m| m.id == meal_id.to_i }
-
-    if meal
-      meal
+  def display_meals(meal_plan)
+    if meal_plan.meals.any?
+      meal_plan.meals.each do |meal|
+        puts "  -> Meal: #{meal.name} (ID: #{meal.id})"
+        puts "     (meal_type: #{meal.meal_type})"
+        puts "     (prep_time: #{meal.prep_time})"
+        puts "     (estimated_cost: #{meal.estimated_cost})"
+        puts "     (calories: #{meal.calories})"
+        puts "     (prepared: #{meal.prepared})"
+        puts "     (favorite: #{meal.favorite})"
+      end
     else
-      puts "Meal not found."
-      nil
+      puts "No meals found for this meal plan."
     end
   end
 
@@ -188,6 +204,22 @@ class CLI
       puts "Success! '{selected_plan.name}' has been deleted."
     else
       puts "Deletion canceled."
+    end
+  end
+
+  def select_meal(meal_plan)
+    display_meals(meal_plan)
+
+    print "Enter a meal ID: "
+    meal_id = gets.chomp
+
+    meal = meal_plan.meals.find { |m| m.id == meal_id.to_i }
+
+    if meal
+      meal
+    else
+      puts "Meal not found."
+      nil
     end
   end
 
